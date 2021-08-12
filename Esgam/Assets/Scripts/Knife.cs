b38986804cb54b3192396777cfbb01dc32fbfde5
ksimaster <ksimasterlord@gmail.com>
2021-08-12 19:58:56 +0300

    Сделана тыкалка в бревно
    Тыкалка недопиленная :)

---
+++ Esgam/Assets/Scripts/Knife.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knife : MonoBehaviour
+{
+    public Rigidbody2D rigidbody;
+    public bool attachedToWood;
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Wood")
+        {
+            rigidbody.velocity = Vector2.zero;
+            transform.parent = col.transform;
+            attachedToWood = true;
+        }
+
+        if (col.gameObject.tag == "Knife")
+        {
+            if(col.gameObject.GetComponent<Knife>().attachedToWood == true)
+            {
+                rigidbody.velocity = Vector2.zero;
+                rigidbody.gravityScale = 1;
+                Debug.LogError("Вы проиграли!!!");
+            }
+        }
+
+        if (col.gameObject.tag == "Apple")
+        {
+            Destroy(col.gameObject);
+            Debug.Log("Поздравляем! Вы выиграли!!!");
+
+        }
+    }
+
+
+
+}
---
+++ Esgam/Assets/Scripts/Wood.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wood : MonoBehaviour
+{
+    public float speed;
+    void FixedUpdate()
+    {
+        transform.Rotate(0,0,speed);
+    }
+}
